e7ca8070b82cee58478cef94425ba0a0bb1d0a32
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2024-04-29 19:43:01 -0600

    Solve exercise 03_reverseString.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    stringArray = str.split("");
+    reverseArray = stringArray.reverse();
+    result = reverseArray.join("");
+    return result;
 };
 
 // Do not edit below this line
