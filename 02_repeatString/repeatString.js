404b5e6080f1d0d7286b51354de86cb05b95959a
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2024-04-29 19:08:16 -0600

    Solve exercie 02_repeatString.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
 
+
+
+const repeatString = function(stringVariable, numRepetitions) {
+    result = "";
+    if (numRepetitions < 0) {
+        result = "ERROR";
+    } else {
+    for (let i=0; i<numRepetitions; i++) {
+        result = result + stringVariable;
+    }
+    }
+    return result;
 };
 
 // Do not edit below this line
