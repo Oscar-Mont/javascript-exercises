b9220aab4624eadaafb06f97ad87fcb696aa5145
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2024-04-30 22:37:00 -0600

    Solve execrise 05_sumAll.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    result = 0;
+    smallestNumber = 0;
+    biggestNumber = 0;
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    } else if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR"
+    } else {
+        if (num1 > num2) {
+            biggestNumber = num1;
+            smallestNumber = num2;
+        } else if (num1 < num2) {
+            biggestNumber = num2;
+            smallestNumber = num1;
+        }
+        do {
+            result = result + smallestNumber;
+            smallestNumber = smallestNumber + 1;
+        } while (smallestNumber <= biggestNumber)
+    }
+    return result;
 };
 
 // Do not edit below this line
