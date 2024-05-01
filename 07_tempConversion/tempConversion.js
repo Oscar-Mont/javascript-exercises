ebdae9d81a85b633feabcabf095c8c38a32d5e75
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2024-05-01 14:01:13 -0600

    Solve 07_tempConversion exercise.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+
+//convert from farenheit to celsius
+const convertToCelsius = function (degreesInFahrenheit) {
+  degreesInCelsius = (degreesInFahrenheit - 32) * (5 / 9);
+  //round to one decimal place by multiplying by 10, rounding and then dividing by 10
+  let result = Math.round(degreesInCelsius * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degreesInCelsius) {
+  degreesInFahrenheit = (degreesInCelsius * (9 / 5) + 32);
+  //round to one decimal place by multiplying by 10, rounding and then dividing by 10
+  let result = Math.round(degreesInFahrenheit * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
