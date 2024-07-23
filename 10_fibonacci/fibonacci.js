3cdc74b8ac2e98f54c7875dc42c822fb7c564639
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2024-07-23 17:41:33 -0600

    Solve exercise 10, fibonacci.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+
+    let cleanNumber;
+    if (typeof number == "string") {
+        cleanNumber = Number(number);
+    } else {
+        cleanNumber = number;
+    }
+
+    fibonacciArray = [1, 1];
+
+    for (let i = 2; i < cleanNumber; i++) {
+        fibonacciArray.push(fibonacciArray[i - 1] + fibonacciArray[i - 2]);
+    }
+
+    if (cleanNumber == 0) {
+        return 0;
+    } else if (cleanNumber < 0) {
+        return "OOPS";
+    } else {
+        return fibonacciArray[number - 1];
+    }
 
 };
 
