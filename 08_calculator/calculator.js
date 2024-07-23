970c993c51a1e413f1212641bc920c95b2d1564f
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2024-07-22 19:11:29 -0600

    Solve exercise 8: calculator.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (...numbers) {
+  return numbers.reduce((accumulator, element) => accumulator + element);
 };
 
-const subtract = function() {
-	
+const subtract = function (...numbers) {
+  return numbers.reduce((accumulator, element) => accumulator - element);
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((accumulator, element) => accumulator + element, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((accumulator, element) => accumulator * element);
 };
 
-const power = function() {
-	
+const power = function (...numbers) {
+  return numbers.reduce((accumulator, element) => Math.pow(accumulator, element));
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0 || number === 1) {
+    return 1;
+  } else {
+    let answer = number;
+    for (let i = number - 1; i > 0; i--) {
+      answer = answer * i;
+    }
+    return answer;
+  }
 };
 
 // Do not edit below this line
