8bcdf5db114ea8f3557918cdfdaa4932a5489583
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2024-07-23 14:37:26 -0600

    Solve exercise 9, palindromes.
    Had to check for the solution. Reviewing some
    very useful array methods was needed.
    Lesson learned.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let validCharacters = "abcdefghijklmnopqrstuvwxyz1234567890"
 
+    let cleanString = str
+        .toLowerCase()
+        .split('')
+        .filter((character) => validCharacters.includes(character))
+        .join('');
+
+    let reverseString = cleanString
+        .split('').reverse().join('');
+
+    return cleanString === reverseString;
 };
 
 // Do not edit below this line
